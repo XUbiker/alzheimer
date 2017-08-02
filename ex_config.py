132848c3650d91d7874ca7e0c8468c5207b732bd
xubiker <xubiker@gmail.com>
2017-08-02 23:51:10 +0300

    Lists and lmdb generation are separated into two notebooks

---
+++ ex_config.py
@@ -0,0 +1,28 @@
+caffe_folder = '/home/xubiker/dev/caffe_modified/'
+
+_codes = {
+    'ternary': { 'NC': 0, 'MCI': 1, 'AD': 2 },
+    'AD-MCI': { 'MCI': 0, 'AD': 1 },
+    'MCI-NC': { 'NC': 0, 'MCI': 1 },
+    'AD-NC': { 'NC': 0, 'AD': 1 },
+}
+
+def get_label_families(label=None):
+    return {
+        None:  ('AD-MCI', 'MCI-NC', 'AD-NC'),
+        'AD':  ('AD-MCI', 'AD-NC'),
+        'MCI': ('AD-MCI', 'MCI-NC'),
+        'NC':  ('AD-NC', 'MCI-NC')
+    }[label]
+
+def get_label_code(label_family, label):
+    return _codes[label_family][label]
+
+def load_caffe(caffe_root=caffe_folder):
+    import sys
+    pcr = caffe_root + "/python"
+    if not pcr in sys.path:
+        sys.path.append(pcr)
+        
+    import caffe
+    print('caffe', caffe.__version__, 'loaded')  
---
+++ ex_utils.py
@@ -0,0 +1,77 @@
+def save_pickle(obj, file_path):
+    import pickle
+    with open(file_path, 'wb') as f:
+        pickle.dump(obj, f)
+
+def get_nii_from_folder(folder):
+    import os
+    res = []
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            if file.endswith('.nii'):
+                res.append(os.path.join(root, file))
+    if len(res) > 1:
+        print('WARNING. Folder %s contains more than one files' % folder)
+    return res
+
+def nii_to_array(nii_filename, data_type, fix_nan=True):
+    import os
+    import nibabel as nib
+    import numpy as np
+    img = nib.load(nii_filename)
+    np_data = img.get_data().astype(data_type)
+    if fix_nan:
+        np_data = np.nan_to_num(np_data)
+    return np_data
+
+def crop_border(data, crop_prc, shift_prc):
+    import numpy as np
+    dims = np.array(data.shape).astype(np.float)
+    pads = np.round(dims * np.array(crop_prc).astype(np.float)).astype(np.int)
+    shifts = np.round(dims * np.array(shift_prc).astype(np.float)).astype(np.int)
+    if pads.size != 3:
+        raise NameError('unsupported number of dimensions')
+    else:
+        x, y, z = data.shape
+        pad_x, pad_y, pad_z = pads
+        sh_x, sh_y, sh_z = shifts
+        data_new = data[sh_x+pad_x:x+sh_x-pad_x, sh_y+pad_y:y+sh_y-pad_y, sh_z+pad_z:z+sh_z-pad_z]
+        print('cropping data:', data.shape, '->', data_new.shape)
+        return data_new    
+
+def augment(data, max_shift, augm_params):
+
+    # augm_params should be a tuple of 4 elements: shift_x, shift_y, shift_z, blur_sigma
+    if data.ndim != 3 or len(augm_params) != 4: raise NameError('invalid input')
+    
+    import numpy as np
+    from scipy.ndimage.filters import gaussian_filter
+    
+    shift_x = augm_params[0]
+    shift_y = augm_params[1]
+    shift_z = augm_params[2]
+    blur_sigma = augm_params[3]
+    
+    s_x, s_y, s_z = (data.shape[0] - 2 * max_shift, data.shape[1] - 2 * max_shift, data.shape[2] - 2 * max_shift)
+
+    blurred = data if blur_sigma == 0 else gaussian_filter(data, sigma = blur_sigma)
+    sub_data = blurred[max_shift + shift_x : s_x + max_shift + shift_x,
+                       max_shift + shift_y : s_y + max_shift + shift_y,
+                       max_shift + shift_z : s_z + max_shift + shift_z]
+    return sub_data    
+    
+def debug_plot_median_slices(np_data, print_slices=False):
+    import matplotlib.pyplot as plt
+    x, y, z = np_data.shape
+    slc = np_data[:, :, z//2]
+    if print_slices: print(slc)
+    plt.matshow(slc, interpolation='nearest', cmap='gray')
+    plt.show()
+    slc = np_data[:, y//2, :]
+    if print_slices: print(slc)
+    plt.matshow(slc, interpolation='nearest', cmap='gray')
+    plt.show()
+    slc = np_data[x//2, :, :]
+    if print_slices: print(slc)
+    plt.matshow(slc, interpolation='nearest', cmap='gray')
+    plt.show()
